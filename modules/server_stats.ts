711838ec17a52c3b8ebff2b5b3eade504815246f
Lucio Delelis <ldelelis@est.frba.utn.edu.ar>
2019-04-12 00:31:23 -0300

    fix(stats): Filter out categories from guild data cache
    TODO: define whether we really want to filter them out or just use them
    to split channels inside the stats embed

--- modules/server_stats.ts
+++ modules/server_stats.ts
@@ -17,7 +17,7 @@ let buildEmbed = async (data) => {
 
 let refreshCache = async (client: Client): Promise<void> => {
   for (let guild of client.guilds) {
-    const clientGuild = guild[1]
+    const clientGuild = guild[1]  // First guild of the array is the current guild
     const guildData = {
       name: clientGuild.name,
       creationDate : clientGuild.createdAt,
@@ -25,14 +25,16 @@ let refreshCache = async (client: Client): Promise<void> => {
         roles: clientGuild.roles.map(x => [x.id, x.name]),
       },
       {
-        channels: clientGuild.channels.map(x => [x.id, x.name]),
+        channels: clientGuild.channels
+          .filter(x => x.type !== 'category')
+          .map(x => [x.id, x.name]),
       }]
     }
     await client['guildData'].set(`${clientGuild.id}.stats`, guildData)
   }
 }
 
-let stats = async (message: Message, _) => {
+let stats = async (message: Message) => {
   const embedMessage = await buildEmbed(message.client['guildData'].get(`${message.guild.id}.stats`))
   return message.channel.send(embedMessage)
 }
